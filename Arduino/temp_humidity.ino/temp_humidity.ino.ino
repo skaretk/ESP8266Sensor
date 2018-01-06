1499d1aff4c5d013f89cd035dbb0c7fcb5575f00
Kjetil Skaret <kskaret@gmail.com>
2018-01-06 18:39:14 +0100

    Add first code with display integration to ssd1306

--- Arduino/temp_humidity.ino/temp_humidity.ino.ino
+++ Arduino/temp_humidity.ino/temp_humidity.ino.ino
@@ -1,6 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 
+// SPI Display SSD1306
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+//----------------------------
 #include "config.h"
 #include "domoticzWifiClient.h"
 #include "espWifiServer.h"
@@ -13,6 +19,16 @@ extern "C" {
 #include <cont.h>
     extern cont_t g_cont;
 }
+
+// SPI Display SSD1306
+#define OLED_RESET 0
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+//----------------------------
+
 
 const char *ssid = "EspSensor";
 
@@ -27,6 +43,13 @@ void setup()
   delay(10);
   // Wait for serial to initialize.
   while(!Serial) { }
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+
+  display.display();
+  delay(500);  
+                                              
   Serial.println("ESP8266 Initliazing..");
 
   if (config->readJson()) {      
@@ -54,6 +77,14 @@ void setup()
           }
       }
       Serial.println("ESP Configured from json!");
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 0);
+      display.println("config.json OK");
+      display.printf("%u sensors added!\n", config->getNoOfSensors());
+      display.display();
+
       wifiClient.connect();
       
       Serial.println("wifiServer started");
@@ -85,14 +116,18 @@ void loop()
         /*Serial.printf("Free heap: %u\n", ESP.getFreeHeap());
         Serial.printf("Free stack: %4d\n", cont_get_free_stack(&g_cont));*/
         // Get readings from all sensors
+        display.clearDisplay();
+        display.setCursor(0, 0);
         for (uint8_t i = 0; i < config->getNoOfSensors(); i++)
         {
             sensor[i]->read();
             sensor[i]->updateData();
             sensor[i]->getSetPointVal();
+            display.printf("%s\n", sensor[i]->data().c_str());            
             /*Serial.printf("Free read: %u\n", ESP.getFreeHeap());
             Serial.printf("Free read: %4d\n", cont_get_free_stack(&g_cont));*/
         }
+        display.display();
         /*Serial.printf("Free after: %u\n", ESP.getFreeHeap());
         Serial.printf("Free after: %4d\n", cont_get_free_stack(&g_cont));*/
     }
